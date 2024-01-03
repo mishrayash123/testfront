fab7be6aeb265c6f0788c11a320a0d2f3802f16e
yash kumar mishra <mishrayash3778@gmail.com>
2024-01-03 19:54:17 +0530

    hjj

--- src/Components/Services/Deals.jsx
+++ src/Components/Services/Deals.jsx
@@ -12,12 +12,14 @@ import {
   import React, { useState, useEffect } from 'react';
   import axios from 'axios';
   import {useNavigate} from 'react-router-dom'
+  import Timer from "../functions/Timer"
 
 
 
 
 function Deals() {
   const [products, setproducts] = useState([]);
+  const [products1, setproducts1] = useState([]);
   const nav = useNavigate();
 
 
@@ -35,8 +37,22 @@ function Deals() {
     }
   }
 
+  const options1 = {
+    method: 'GET',
+  url: "https://fakestoreapi.com/products/category/women's clothing",
+  };
+
+  const fetchData1 = async () => {
+    try {
+      const response = await axios.request(options1);
+      setproducts1(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
-    
+    fetchData1();
     fetchData();
   }, []);
 
@@ -87,7 +103,8 @@ function Deals() {
     </div>
 </section>
 <div className='my-3'>
-        <h2 className="font-bold text-center text-2xl">Top Deals (Ends in : 23:30:20)</h2>
+        <h2 className="font-bold text-center text-2xl">Top Deals</h2>
+        <Timer />
         <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 pt-3 gap-8 w-[90%]" role="group">
          {
           products.map(products =>(
@@ -111,9 +128,14 @@ function Deals() {
           </Typography>
         </div>
         <div className='flex items-center justify-between'>
-        <Typography color="gray" className=' text-black font-bold' placeholder="k">
+        <div className="flex flex-row gap-1">
+        <Typography color="gray" className='line-through text-red-600 font-bold text-sm' placeholder="k">
+          {parseInt(products.price+50)}
+        </Typography>
+        <Typography color="gray" className=' text-black font-bold text-sm' placeholder="k">
           {products.price} &#8377;
         </Typography>
+        </div>
         <Typography
           placeholder="k"
             color="blue-gray"
@@ -133,6 +155,73 @@ function Deals() {
             </svg>
             4.5
           </Typography>
+        </div>
+        {/* <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
+        </div> */}
+      </CardBody>
+    </Card>
+    </a>
+          ))
+         }
+     </div>
+      </div>
+      <div className='my-3'>
+        <h2 className="font-bold text-center text-2xl">Deals of the day</h2>
+        <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 pt-3 gap-8 w-[90%]" role="group">
+         {
+          products1.map(products =>(
+            <a href='' onClick={
+              (e) => {
+                nav('/product', { state: { id: products.id,isincart:false} });
+              }
+          }>
+            <Card className="cardwid shadow-lg m-2" placeholder="k">
+      <CardHeader floated={false} color="blue-gray" placeholder="k">
+        <img
+          src={products.image}
+          alt="ui/ux review check"
+          className='rounded-lg wid'
+        />
+      </CardHeader>
+      <CardBody placeholder="k">
+        <div className="mb-3 ">
+          <Typography variant="h6" color="blue-gray" className="font-medium" placeholder="k">
+           {products.title.slice(0,20)}
+          </Typography>
+        </div>
+        <div className='flex items-center justify-between'>
+          <div className="flex flex-row gap-1">
+        <Typography color="gray" className='line-through text-red-600 font-bold text-sm' placeholder="k">
+          {parseInt(products.price+50)}
+        </Typography>
+        <Typography color="gray" className=' text-black font-bold text-sm' placeholder="k">
+          {products.price} &#8377;
+        </Typography>
+        </div>
+        <Typography
+          placeholder="k"
+            color="blue-gray"
+            className="flex items-center gap-1.5 font-normal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="-mt-0.5 h-5 w-5 text-yellow-700"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                clipRule="evenodd"
+              />
+            </svg>
+            4.5
+          </Typography>
+        </div>
+        <div className='flex items-center justify-between'>
+        <p className=' text-red-600 font-bold text-sm' placeholder="k">
+          Deal Of the day
+        </p>
         </div>
         {/* <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
         </div> */}
