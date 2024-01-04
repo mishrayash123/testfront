4b453493f5d2ca24d152f476a4edd3ceb513634f
yash kumar mishra <mishrayash3778@gmail.com>
2024-01-04 21:07:58 +0530

    jk

--- src/Components/Seller/Addproducts.jsx
+++ src/Components/Seller/Addproducts.jsx
@@ -1,49 +1,58 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {storage} from "../Firebase/firebase.config";
+import {ref, uploadBytes, getDownloadURL} from "firebase/storage"
 
 function Edit() {
   const [title, settitle] = useState("");
   const [price, setprice] = useState(0);
   const [description, setdescription] = useState("");
-  const [image, setimage] = useState(null);
+  const [image, setimage] = useState("");
+  const [image1, setimage1] = useState(null);
   const [textoimage, settextoimage] = useState("");
   const [error, setError] = useState(""); // Track form validation error
   const navigate = useNavigate();
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setimage(file);
-    settextoimage("Image selected");
-  };
+  const handleuploadimage = async() =>{
+    const imageRef = ref(storage, title);
+    if (image1) {
+        uploadBytes(imageRef, image1).then(() => {
+            getDownloadURL(imageRef).then((url) => {
+                setimage(url);
+                alert("Image Uploaded")
+                settextoimage(" ")
+            }).catch((error) => {
+                console.log(error.message, "error geting the image url");
+            })
+            setimage(null);
+            settextoimage(" ")
+        }).catch((error) => {
+            console.log(error.message);
+        })
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const jwt = localStorage.getItem("token");
-    const id = localStorage.getItem("userId");
-
-    
-
+    setprice(parseInt(price))
+    const userid = localStorage.getItem("userId");
     try {
       const response = await fetch(
-        `https://start1-backend.onrender.com/auth/signUp/${id}`,
+        `https://e-cartbackend.onrender.com/addtoproducts`,
         {
-          method: "PUT",
+          method: "POST",
           headers: {
-            Authorization: `Bearer ${jwt}`,
+            "Content-Type": "application/json",
           },
-          body: [],
+          body: JSON.stringify({title,userid,price,description,image}),
         }
       );
 
       if (response.ok) {
         // Handle successful profile update
-        alert("Profile updated successfully");
-        navigate("/");
+        alert("Added successfully");
       } else {
-        // Handle errors
-        const data = await response.json();
-        console.error("Error updating profile:", data);
+        alert("Something wents wrong")
       }
     } catch (error) {
       console.error("Error during profile update:", error);
@@ -62,7 +71,7 @@ function Edit() {
             id="mobile"
             name="mobile"
             value={title}
-            onChange={(e) => settextoimage(e.target.value)}
+            onChange={(e) => settitle(e.target.value)}
             required // Mark the field as required
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
@@ -120,19 +129,25 @@ function Edit() {
           </label>
           <input
             type="file"
-            id="image"
-            name="image"
-            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            onChange={handleImageChange}
-          />
+            className="block w-full text-sm rounded-lg cursor-pointer"
+            onChange={(e) => {
+              if (e.target.files[0]) {
+                setimage1(e.target.files[0]);
+    settextoimage("Image selected");
+    handleuploadimage();
+            }
+  }}/>
         </div>
         <button
           type="submit"
           className="text-white bg-blue-700 hover-bg-blue-800 focus-ring-4 focus-outline-none focus-ring-blue-300 font-medium rounded-lg text-sm w-full sm-w-auto px-5 py-2.5 text-center dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800"
-        >
+       onClick={(e)=>{
+        handleSubmit(e);
+       }}
+       >
           Add Product
         </button>
-        {textoimage && <p>{textoimage}</p>}
+        {textoimage && <p className="text-red-500">{textoimage}</p>}
         {error && <p className="text-red-500">{error}</p>}
       </form>
     </div>
